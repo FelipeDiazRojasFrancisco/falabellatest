8314c3c153ea530670ffdc1bbd1efa09531bf7a7
FelipeDiazRojasFrancisco <85212384+FelipeDiazRojasFrancisco@users.noreply.github.com>
2021-06-02 02:17:29 -0400

    Add files via upload

---
+++ test/testApp.js
@@ -0,0 +1,44 @@
+const assert = require('chai').assert;
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+//Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
+
+describe('App', function(){
+
+    describe('sayHello()',function(){
+         //Evaluaque en app exista la palabra
+     it ('sayHello should return hello', function(){
+        //let result = sayHello();
+        assert.equal(sayHelloResult,'hello');
+
+    });
+    //Evalua que el tipo sea un string
+    it ('sayHello should return type string', function(){
+        //let result = sayHello();
+        assert.typeOf(sayHelloResult, 'string');
+    });
+
+    });
+
+    describe('addNumber()',function(){
+
+         //Evalua que la operacion matematica de como resultado 5
+     it ('addNumbers should be above 5', function(){
+        //let result = addNumbers (5,5);
+        assert.isAbove(addNumbersResult, 5);
+    });
+    //Evalua que el tipo sea number
+    it ('addNumber should return type number', function(){
+       //let result = addNumbers(5,5);
+       assert.typeOf(addNumbersResult, 'number');
+   });
+
+    });
+   
+    
+
+});
